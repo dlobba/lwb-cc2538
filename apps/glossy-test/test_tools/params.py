e274da394bee0f0043fb724fe6b969ac095e5a9f
dlobba <diego.lobba@studenti.unitn.it>
2019-12-20 13:01:55 +0100

    removed old testing info

--- apps/glossy-test/test_tools/params.py
+++ apps/glossy-test/test_tools/params.py
@@ -1,19 +1,17 @@
 PARAMS = {
     "app_folder": "../",                 # Path where the glossy_test.c file is
-    "sims_dir": "./simulations",         # Path where to store simulations in
+    "sims_dir": "./",         # Path where to store simulations in
 
     # list of nodes available
-    # "nodes" : [1, 2, 3, 4 ,5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
-    "nodes" : [1, 2, 3, 4 ,5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    "nodes" : [],
 
     # list describing, in each simulation, the node to be the initiator.
     # There will be a **separate** simulation for each of these nodes.
-    #"initiator" : [1, 2, 3, 4 ,5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
-    "initiator" : [17],
+    "initiator" : [],
 
     "ntxs"     : [2],
     "payloads" : [2],
-    "powers"   : [0xb6],                # Default tx power for cc2538
+    "powers"   : [-24, -15, -13, -11, -9, -7, -5, -3, -1, 0],                # Default tx power for cc2538
 
     # If a time is given in format %Y-%m-%d %H:%M
     # then the first simulation is scheduled at that time
@@ -22,5 +20,5 @@ PARAMS = {
     # Time to schedule the simulation.
     "ts_init"  : "asap",
 
-    "duration" : 600                    # Duration of each simulation
+    "duration" : 120                    # Duration of each simulation
 }
